050803ef9a5248ec0de73f1f2f2c909318849609
Alexei Lubinets <lubinets01@mail.ru>
2024-12-25 22:44:07 +0500

    Выполнено третье задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,28 @@
+// Задача 1
+
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false; 
+    }
+
+    
+    return arr1.every((value, index) => value === arr2[index]);
 }
 
+// Задача 2
+  
 function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter(user => user.gender === gender);
+
+    
+    if (filteredUsers.length === 0) {
+        return 0;
+    }
+
+    
+    const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+    
+    
+    return (totalAge / filteredUsers.length);
   
 }
