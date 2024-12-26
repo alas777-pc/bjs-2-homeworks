7dd7e4147b0b08580230365b0b6f13fa27f6f60a
Alexei Lubinets <lubinets01@mail.ru>
2024-12-26 23:02:20 +0500

    Выполнено четвертое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+// Метод установки предмета
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+// Метод добавления нескольких оценок
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (!this.marks || this.excluded) {
+        console.error("Студент отчислен или отсутствует массив оценок.");
+        return;
+    }
+    
+    for (const mark of marksToAdd) {
+        this.marks.push(mark);
+    }
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+// Метод вычисления среднего балла
+Student.prototype.getAverage = function() {
+    if (!this.marks || !this.marks.length) {
+        return 0;
+    }
+    
+    const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks.length;
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+// Метод отчисления студента
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+};
