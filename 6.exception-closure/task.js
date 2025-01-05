ab899d364f61ed90276d82258109dfc6b30ead9a
Alexei Lubinets <lubinets01@mail.ru>
2025-01-05 22:38:21 +0500

    Выполнено шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,57 @@
-﻿
+﻿// Задача 1
+
+function parseCount(value) {
+    const result = Number.parseFloat(value);
+    
+    if (Number.isNaN(result)) {
+        throw new Error("Невалидное значение"); // Генерация ошибки, если результат NaN
+    }
+    
+    return result; // Возвращаем результат парсинга
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value); // Пытаемся распарсить значение
+    } catch (error) {
+        return error; // Возвращаем ошибку, если парсинг не удался
+    }
+}
+
+// Задача 2
+
+class Triangle {
+    constructor(a, b, c) {
+      if (a + b <= c || a + c <= b || b + c <= a) {
+        throw new Error('Треугольник с такими сторонами не существует');
+      }
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+  
+    get perimeter() {
+      return this.a + this.b + this.c;
+    }
+  
+    get area() {
+      const p = this.perimeter / 2;
+      const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+      return parseFloat(area.toFixed(3)); // Округляем до трёх знаков
+    }
+  }
+  
+  function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (e) {
+      return {
+        get perimeter() {
+          return 'Ошибка! Треугольник не существует';
+        },
+        get area() {
+          return 'Ошибка! Треугольник не существует';
+        }
+      };
+    }
+  }
